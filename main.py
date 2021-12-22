9747ac0ed4b2b57c097e7cd15c015186e62e1b64
Paul Pham <cryptogoth@Pauls-MacBook-Pro.local>
2021-12-21 17:50:05 -0700

    First commit of Discord cheers bot 🥳

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+import os
+import dotenv
+import requests
+import json
+
+client = discord.Client()
+
+dotenv.load_dotenv()
+
+GUILD = os.getenv('DISCORD_GUILD')
+my_guild = None
+
+response = requests.get('https://api.freecodecamp.org/api/users/get-public-profile?username=paulpham')
+profile = json.loads(response.text)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            my_guild = guild
+            break
+
+    if my_guild is None:
+        exit(f'Our guild {GUILD} was not found.')
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to Arcology Builders and the Detroit FreeCodeCamp Meetup'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await message.channel.send('I am the One.')    
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(os.getenv('DISCORD_TOKEN'))
